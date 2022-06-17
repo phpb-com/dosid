ed26c6d9fb62fc3daa8c9f2abd174550dda1dce1
Ian Matyssik <ian@phpb.com>
2022-06-18 08:05:01 +0900

    chore: more comments and lint related staff

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['standard'],
+  extends: ['standard', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
--- src/counter.ts
+++ src/counter.ts
@@ -4,25 +4,29 @@ import { hash } from 'ohash' // Import library for consistent hashing
 export interface Env {}
 
 export class DOSIDCounter {
-  state: DurableObjectState
-  myId: string
+  state: DurableObjectState // DO state object
+  myId: string // DO object ID string
+  env: Env // DO environment variables
 
   constructor (state: DurableObjectState, env: Env) {
     this.state = state
     this.myId = state.id.toString()
+    this.env = env
   }
 
   // Handle HTTP requests from clients.
   async fetch (request: Request) {
+    // Parse the request URL
     const url = new URL(request.url)
-    // Returned numeric ID will consist of three parts (left to right):
-    // 1) Durable Object stored counter value
-    // 2) 9 bits (512 values) of shard value (shard id calculation should be based on physical location of DO)
-    // 3) 7 bits (128 values) of crypto randomness that will defined stored value id of the counter
+
+    /* Returned numeric ID will consist of three parts (left to right):
+       1) Durable Object stored counter value
+       2) 9 bits (512 values) of shard value (shard id calculation should be based on physical location of DO)
+       3) 7 bits (128 values) of crypto randomness that will defined stored value id of the counter */
 
     // 3) Durable Object stored counter value ID
-    // Generate random id tail, 7 bits
-    // Generate random id tail that will be used to store the counter value
+    // Generate random id tail that will be used to store the counter value,
+    // and use only last 7 bits (0 - 127). This gives us 128 counters per DO shard
     const randomVal = crypto.getRandomValues(new Uint8Array(1))
     const idTail = BigInt(randomVal[0] & ((1 << 7) - 1) /* mask 7 bits */)
 
@@ -33,9 +37,11 @@ export class DOSIDCounter {
     // Read the counter value from durable storage / cache, or initialize it to 0
     let counterValue: bigint =
       (await this.state.storage?.get(idTail.toString())) || 0n
+
     // Increment and store the counter value, meaning that we will never use 0 as a value
     await this.state.storage?.put(idTail.toString(), ++counterValue)
 
+    // Return debug info, if requested
     if (url.pathname === '/debug') {
       return new Response(
         JSON.stringify({
